14891f2441df89d871b12346611d994cb0a0b8ad
josh-un1b3 <joshua.felder@students.unibe.ch>
2019-12-10 23:31:02 +0100

    adjusted home routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,8 +3,8 @@ import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
 import {RoleGuardService} from './auth/auth-guard-service/roleguard.service';
 
 const routes: Routes = [
-   {path: 'home', redirectTo: '', pathMatch: 'full'},
-   {path: '', loadChildren: () => import('./pages/home/home.module').then(m => m.HomePageModule)},
+   {path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomePageModule)},
+   {path: '', redirectTo: '/home', pathMatch: 'full'},
    {path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginPageModule)},
    {path: 'register', loadChildren: './pages/register/register.module#RegisterPageModule'},
    {
