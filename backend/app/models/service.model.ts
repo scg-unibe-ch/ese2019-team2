b215a5c166466fd55b3a4316186c2a139c313ede
josh-un1b3 <joshua.felder@students.unibe.ch>
2019-12-10 13:29:28 +0100

    fix backend

--- backend/app/models/service.model.ts
+++ backend/app/models/service.model.ts
@@ -5,134 +5,65 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 let ServiceSchema = new Schema({
-   < <<<<<< HEAD
-      category
-:
-{
-   type: String
-}
-,
-subCategory: {
-   type: String
-}
-,
-userID: {
-   type: String
-}
-,
-username: {
-   type: String
-}
-,
-serviceName: {
-   type: String
-}
-,
-img: {
-   type: Array
-}
-,
-price: {
-   type: Number
-}
-,
-description: {
-   type: String
-}
-,
-location: {
-   type: String
-}
-,
-maxPeople: {
-   type: Number
-}
-,
-street: {
-   type: String
-}
-,
-city: {
-   type: String
-}
-,
-zip: {
-   type: String
-}
-,
-phone: {
-   type: String
-}
-,
-rating: {
-   type: Number
-}
-======
-=
-   category
-:
-{
-   type: String
-}
-,
-subCategory: {
-   type: String
-}
-,
-userID: {
-   type: String
-}
-,
-username: {
-   type: String
-}
-,
-serviceName: {
-   type: String
-}
-,
-img: {
-   type: String
-}
-,
-price: {
-   type: Number
-}
-,
-description: {
-   type: String
-}
-,
-location: {
-   type: String
-}
-,
-maxPeople: {
-   type: Number
-}
-,
-street: {
-   type: String
-}
-,
-city: {
-   type: String
-}
-,
-zip: {
-   type: String
-}
-,
-phone: {
-   type: String
-}
-,
-rating: {
-   type: Number
-}
->>>>>>>
-profil - edit
+   category:
+      {
+         type: String
+      }
+   ,
+   subCategory: {
+      type: String
+   }
+   ,
+   userID: {
+      type: String
+   }
+   ,
+   username: {
+      type: String
+   }
+   ,
+   serviceName: {
+      type: String
+   }
+   ,
+   img: {
+      type: Array
+   }
+   ,
+   price: {
+      type: Number
+   }
+   ,
+   description: {
+      type: String
+   }
+   ,
+   location: {
+      type: String
+   }
+   ,
+   maxPeople: {
+      type: Number
+   }
+   ,
+   street: {
+      type: String
+   }
+   ,
+   city: {
+      type: String
+   }
+   ,
+   zip: {
+      type: String
+   }
+   ,
+   phone: {
+      type: String
+   }
+   ,
+   rating: {
+      type: Number
+   }
 })
-;
-
 module.exports = mongoose.model('Service', ServiceSchema);
