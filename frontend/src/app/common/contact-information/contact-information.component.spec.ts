b4dc43f7c97c0775c974ab223bc4616a57549863
josh-un1b3 <joshua.felder@students.unibe.ch>
2019-12-10 22:53:28 +0100

    detail style

--- frontend/src/app/common/contact-information/contact-information.component.spec.ts
+++ frontend/src/app/common/contact-information/contact-information.component.spec.ts
@@ -2,6 +2,8 @@ import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {ContactInformationComponent} from './contact-information.component';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {RouterTestingModule} from '@angular/router/testing';
 
 describe('ContactInformationComponent', () => {
    let component: ContactInformationComponent;
@@ -10,6 +12,7 @@ describe('ContactInformationComponent', () => {
    beforeEach(async(() => {
       TestBed.configureTestingModule({
          declarations: [ContactInformationComponent],
+         imports: [HttpClientTestingModule, RouterTestingModule],
          schemas: [CUSTOM_ELEMENTS_SCHEMA],
       })
          .compileComponents();
